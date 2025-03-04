feaf156301be9f077f1e9fdec253481120eca51b
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-03-04 15:14:50 +0530

    set added

---
+++ day01/longestUniqueSubstring.js
@@ -0,0 +1,14 @@
+const longestUniqueSubstring=(text)=>{
+let characters=new Set();
+let max=0;
+
+for(let i=0;i<text.length;i++){
+    characters.add(text[i]);
+    max=characters.size
+}
+console.log(max)
+}
+longestUniqueSubstring("abcabcbb"); // Output: 3 ("abc")
+longestUniqueSubstring("bbbbb");   // Output: 1 ("b")
+longestUniqueSubstring("pwwkew");  // Output: 3 ("wke")
+longestUniqueSubstring("");        // Output: 0
