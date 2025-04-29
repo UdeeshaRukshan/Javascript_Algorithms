10601a0ba8cdb848a6995c470461e8a8f106c158
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-04-29 21:15:53 +0530

    vowels

---
+++ day03/index.js
@@ -0,0 +1,14 @@
+function result(str) {
+    let count = 0;
+    const vowels = "aeiouAEIOU";
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  const out = result("Hello World This Is Udeesha Rukshan Gamage!");
+  console.log(out);  // Output: number of vowels
+  
